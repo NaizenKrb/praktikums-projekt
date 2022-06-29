0b02e6fe8143e7e1a77775d481a68da3bb96f0fb
Niclas Heide <niclas.heide@students.tbs1.de>
2022-06-29 16:07:21 +0200

    changed to new script completely and removed old one

--- workspace/js/script.js
+++ workspace/js/script.js
@@ -4,12 +4,13 @@ const months = ["January","February","March","April",
                 "May","June","July","August","September",
                 "October","November","December"];
 
-const weekDays = ["Sunday", "Monday","Tuesday","Wednesday",
-                "Thursday","Friday","Saturday"];
+const weekDays = ["Sonntag", "Montag","Dienstag","Mittwoch",
+                "Donnerstag","Freitag","Samstag"];
 
 let date = new Date();
 let currentYear = date.getFullYear();
 let currentDay = date.getDate();
+
 let monthIndex = date.getMonth();
 let currentMonth = months[monthIndex];
 
@@ -17,14 +18,16 @@ let daysInCurrentMonth = getDaysInMonth(currentYear, monthIndex);
 let weekDay = getWeekDay(currentYear, monthIndex, currentDay);
 
 let buttonContainer = document.querySelectorAll(".buttoncontainer");
+
 buttonContainer.forEach((entry) =>{
     entry.querySelector('.viewButton').addEventListener('click', () => {
         let menu = entry.querySelector(".viewMenu");
+        
         if (menu.classList.contains("hidden")) {
             menu.classList.remove("hidden");
         } else {
             menu.classList.add("hidden");
-        };
+        }
     });         
 });
 
@@ -32,6 +35,7 @@ const thisMonthButton = document.querySelectorAll(".thisMonth");
 thisMonthButton.forEach((entry) =>{
     entry.addEventListener("click", () => {
         if(monthIndex !== date.getMonth() || currentYear !== date.getFullYear()) {
+            console.log("Month changed");
             monthIndex = date.getMonth();
             currentYear = date.getFullYear();
             document.querySelector("#days").innerHTML = "";
@@ -52,14 +56,14 @@ if (nextButtonClicked != null) {
     nextButtonClicked.addEventListener("click", function() {
         setNextMonth();
     });
-};
+}
 
 const lastButtonClicked = document.getElementById("last")
 if (lastButtonClicked != null) {
     lastButtonClicked.addEventListener("click", function() {
         setLastMonth();
     });
-};
+}
 
 // Modal Part ---------------------------------------------------------------
 let openmodal = document.querySelectorAll(".addEvent");
@@ -73,22 +77,25 @@ openmodal.forEach((entry) =>{
 const overlay = document.querySelector('.modal-overlay');
 overlay.addEventListener('click', toggleModal);
 
-var closemodal = document.querySelectorAll('.modal-close')
-for (var i = 0; i < closemodal.length; i++) {
+const closemodal = document.querySelectorAll('.modal-close')
+for (let i = 0; i < closemodal.length; i++) {
     closemodal[i].addEventListener('click', toggleModal);
-};
+}
 
 document.onkeydown = function(evt) {
     evt = evt || window.event
-    var isEscape = false
+    let isEscape = false
     if ("key" in evt) {
         isEscape = (evt.key === "Escape" || evt.key === "Esc");
     } else {
         isEscape = (evt.keyCode === 27);
-    };
+    }
     if (isEscape && document.body.classList.contains('modal-active')) {
         toggleModal();
-    };
+    }
+    if (isEscape && document.body.classList.contains('overflow-hidden')) {
+        toggleModal();
+    }
 };
 
 function toggleModal () {
@@ -97,160 +104,155 @@ function toggleModal () {
     modal.classList.toggle('opacity-0');
     modal.classList.toggle('pointer-events-none');
     body.classList.toggle('modal-active');
-};
+    body.classList.toggle('overflow-hidden');
+}
 // End of Modal Part ---------------------------------------------------------
+const addButton = document.querySelector(".addButton");
+addButton.addEventListener("click", addEvent);
+
+let jsonEvent;
+
+let jsonEventList = "events" in localStorage? JSON.parse(localStorage.getItem('events')) : {};
+
+function addEvent() {
+    let name = document.querySelector(".name").value;
+    let startDate = document.querySelector(".startDate").value;
+    let endDate = document.querySelector(".endDate").value;
+    
+    jsonEventList[`${name}`] = {name: name, start: startDate, end: endDate};
+    localStorage.setItem("events", JSON.stringify(jsonEventList));
+
+    console.log(localStorage.getItem('events'));
+    console.log(jsonEventList);
+    console.log("spacer");
+    //toggleModal();
+}
 
 // Get all the Dates + the Weekday
 function getDaysPlusWeekday(monthIndex, year) {
-    const days = getDaysInMonth(year, monthIndex);
-    let daysLastMonth = getDaysInMonth(year, monthIndex - 1);
-    let daysNextMonth = getDaysInMonth(year, monthIndex + 1);
-    let firstDayOfMonth = getWeekDay(year, monthIndex, 1);
-    let lastDayOfMonth = getWeekDay(year, monthIndex, days);
-    let colStart,
-        rowStart;
 
-    if(firstDayOfMonth !== "Monday") {
-         colStart = weekDays.indexOf(firstDayOfMonth) - 1;
-    } else {
-         colStart = 0;
-    };
+    // Start-Datum mit Jahr, Monat und dem 1. Tag
+    const startDate = new Date(year, monthIndex, 1);
 
-    let colStartLastMonth;
-    let colStartNextMonth;
+    // End-Datum, der "Nullte Tag" des folgenden Monats, ein kleiner Trick um einen Tag in die 
+    // Vergangenheit zu springen und so das richtige End-Datum des Monats zu erhalten.
+    const endDate = new Date(year, monthIndex + 1, 0);
 
-    let getFillDaysOfLastMonth = daysLastMonth;
-    let output = [];
+    // Wenn das Start-Datum nicht bei Montag anfängt, dann gehen wir die notwendigen Tage mit einem
+    // negativen Wert bei der `setDate` methode zurück. Das Problem hierbei: JavaScript bietet mit 
+    // `getDay()` zwar eine Möglichkeit an den Wochentag als nummerischen Wert zu erhalten, nutzt
+    // allerdings die folgenden Werte:
+    //      0 = Sonntag
+    //      1 = Montag
+    //      2 = Dienstag
+    //      3 = Mittwoch
+    //      4 = Donnerstag
+    //      5 = Freitag
+    //      6 = Samstag
+    // da wir allerdings bei Montag anfangen möchten, müssen wir diese Werte sortieren, und können
+    // dadurch die Anzahl der vorherigen Tage ermitteln (-1, da wir beim aktuellen startDate
+    // Objekt in die Vergangenheit springen möchten und daher 0-index basiert arbeiten):
+    if (startDate.getDay() !== 1) {
+        startDate.setDate(-([1, 2, 3, 4, 5, 6, 0].indexOf(startDate.getDay()) - 1));
+    }
 
-    function fillWithPreviousMonth() {
-        for (let i = colStart - 1; i >= 0; i--) {
+    // Ähnlich können wir auch das Ende unseres Kalenders berechnen, müssen allerdings die Anzahl
+    // der nachfolgenden Tage (bis Sonntag) auf das aktuelle End-Datum draufrechnen. Um diese Zahl 
+    // zu ermitteln, rechnen wir die Position in unserem "sortierten Wochentag-array" gegen die
+    // Gesamtanzahl der Wochentage. (7, allerdings 0-index basiert... also 6).
+    if (endDate.getDay() !== 0) {
+        endDate.setDate(
+            endDate.getDate() + 
+            (6 - [1, 2, 3, 4, 5, 6, 0].indexOf(endDate.getDay()))
+        );
+    }
 
-            colStartLastMonth = i;
-            // Tageszahl für die Füllung vom letzten Monat
-            let lastMonthDay = getFillDaysOfLastMonth + ",";
-            output.push(
-                `
-                <div class="col-start-${colStartLastMonth + 1} col-span-1 row-start-${1} border-r border-b border-slate-600 bg-slate-400">
-                    <div class="py-1 px-3 border-b border-slate-600 bg-slate-500 text-gray-800 truncate">
-                    ${lastMonthDay.split(",")[0]}.
-                    ${lastMonthDay.split(",")[1]}
-                    </div>
-                    <div class="py-1 h-24 min-h-[12rem]"></div>
-                </div>
-            `);
-            getFillDaysOfLastMonth--;
-        };
-    };
-    fillWithPreviousMonth();
-    output.reverse();
-    document.querySelector("#days").innerHTML += output.join("");
+    // Jetzt wo startDate und endDate den Kalender des entsprechenden Monats enthält + die Wochen-
+    // tage am Anfang sowie am Ende ergänzt haben, können wir mit einem kleinen Trick sämtliche 
+    // Daten in einer einzigen While Schleife ausgeben:
+    // Die `toJSON` Methode gibt einen DateTime String zurück, die ersten 10 Zeichen enthalten das
+    // Datum als "[JAHR]-[MONAT]-[TAG]", welches wir mit `≤` gut mit dem Ende vergleichen können:
+    const output = [];
+    const until = endDate.toJSON().slice(0, 10);        // Da sich endDate nicht ändern, können wir es als Konstante festlegen
+    while(startDate.toJSON().slice(0, 10) <= until) {
 
-    for (let i = 1; i <= days; i++) {
-        const weekDay = getWeekDay(year, monthIndex, i);
-        let date = i;
-        let day = weekDay + ", " + date;
-        const weekEnd = weekDay === "Saturday" || weekDay === "Sunday";
+        // Der aktuelle Monat / das aktuelle Jahr, basierend auf die Funktions-Argumente
+        const current = startDate.getMonth() === monthIndex && startDate.getFullYear() === year;
 
-        if ((i + colStart) > 7 && (i + colStart) < 15) {
-            rowStart = 2;
-        } else if((i + colStart) > 14 && (i + colStart) < 22) {
-            rowStart = 3;
-        } else if((i + colStart) > 21 && (i + colStart) < 29) {
-            rowStart = 4;
-        } else if((i + colStart) > 28 && (i + colStart) < 36) {
-            rowStart = 5;
-        } else if((i + colStart) > 35) {
-            rowStart = 6;
-        } else{
-            rowStart = 1;
-        };
+        const isWeekEnd = startDate.getDay() === 6 || startDate.getDay() === 0;
 
-        if (colStart === 0) {
-            let output = weekEnd == true ?
-                `
-                <div class="col-span-1 row-start-${rowStart} border-r border-b border-slate-600 bg-slate-300">
-                    <div class="py-1 px-3 border-b border-slate-600 bg-slate-400 text-gray-800 truncate">
-                    ${day.split(",")[1]}.
-                    ${day.split(",")[0]}
-                    </div>
-                    <div class="py-1 h-24 min-h-[12rem]"></div>
-                </div>
-                `
-                :
-                `
-                <div class="col-span-1 row-start-${rowStart} border-r border-b border-slate-600 bg-slate-100">
-                    <div class="py-1 px-3 border-b border-slate-600 bg-slate-200  truncate">
-                        ${day.split(",")[1]}.
-                        ${day.split(",")[0]}
+        // Die folgende Funktion "übersetzt" das aktuelle Date-Object auf Österreichisch (um die Kollegen zu ärgern).
+        // Intl.DateTimeFormat -> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat
+        const dayFormat = new Intl.DateTimeFormat('de-AT', {
+            day: 'numeric',
+            weekday: 'short',
+            month: 'short'
+        }).format(startDate);
+
+
+        // HTML Inhalt
+        if(isWeekEnd && current) {
+            if(startDate.getDay() === 6) {
+                output.push(`
+                <div class="col-span-1 border-r border-slate-600 bg-slate-300">
+                    <div class="py-1 px-3 border-y border-slate-600 bg-slate-400 text-gray-500 truncate">
+                        ${dayFormat}
                     </div>
                     <div class="py-1 min-h-[12rem] break-words">
-                        "Here you can add some content"
                     </div>
                 </div>
-            `;
-            document.querySelector("#days").innerHTML += output;
+                `);
+            } else {
+                output.push(`
+                <div class="col-span-1 bg-slate-300">
+                    <div class="py-1 px-3 border-y border-slate-600 bg-slate-400 text-gray-500 truncate">
+                        ${dayFormat}
+                    </div>
+                    <div class="py-1 min-h-[12rem] break-words">
+                    </div>
+                </div>
+                `);
+            }
         } else {
-            if(colStart + i === 0) {
-                colStart = 6;
-                fillWithPreviousMonth();
-                output.reverse();
-                document.querySelector("#days").innerHTML += output.join("");
-            };
-            output = weekEnd == true ?
+            output.push(
+            current?
                 `
-                <div class="col-span-1 row-start-${rowStart} border-r border-b border-slate-600 bg-slate-300">
-                    <div class="py-1 px-3 border-b border-slate-600 bg-slate-400 text-gray-800 truncate">
-                    ${day.split(",")[1]}.
-                    ${day.split(",")[0]}
+                <div class="col-span-1 border-r border-slate-600 bg-slate-50">
+                    <div class="py-1 px-3 border-y border-slate-600 bg-slate-200  truncate">
+                        ${dayFormat}
                     </div>
-                    <div class="py-1 h-24 min-h-[12rem]"></div>
-                </div>
-                `
-                :
-                `
-                <div class="col-span-1 row-start-${rowStart} border-r border-b border-slate-600 bg-slate-50">
-                    <div class="py-1 px-3 border-b border-slate-600 bg-slate-200  truncate">
-                        ${day.split(",")[1]}.
-                        ${day.split(",")[0]}
-                    </div>
-                    <div class="py-1 min-h-[12rem] break-words">
+                    <div class="vacationInput py-1 min-h-[12rem] break-words">
                         "Here you can add some content"
                     </div>
                 </div>
-                `;
-            };
-        document.querySelector("#days").innerHTML += output;
-    };
-    // Fill with Next Month -----------------------------------------------------
-    output = [];
-    colStart = 6 - weekDays.indexOf(lastDayOfMonth);
-    if(colStart != 6) {
-        for(let i = 0; i <= colStart; i++){
-            let date = i + 1;
-            output.push(
+            `:
                 `
-                <div class="border-r border-b border-slate-600 bg-slate-400">
-                    <div class="py-1 px-3 border-b border-slate-600 bg-slate-500 text-gray-800 truncate">
-                    ${date}.
+                <div class="col-span-1 border-r border-slate-600 bg-slate-400">
+                    <div class="py-1 px-3 border-y border-slate-600 bg-slate-500 text-gray-800 truncate">
+                        ${dayFormat}
                     </div>
                     <div class="py-1 h-24 min-h-[12rem]"></div>
                 </div>
-                `
-            );
-        };
-        document.querySelector("#days").innerHTML += output.join("");
-    };
-    // --------------------------------------------------------------------------
-};
+            `)
+        }
+
+        // Der While-Loop ändert die bestehenden Vergleichswerte nicht, daher müssen wir das tun:
+        startDate.setDate(startDate.getDate() + 1);
+    }
+
+    document.querySelector("#days").innerHTML += output.join("");
+}
 
 function getDaysInMonth(year, month) {
     return new Date(year, month + 1, 0).getDate();
-};
+}
+
 
 function getWeekDay(year, month, day) {
     let date = new Date(year, month, day);
+
     return weekDays[date.getDay()];
-};
+}
 
 function setNextMonth() {
     if (monthIndex === 11) {
@@ -262,7 +264,7 @@ function setNextMonth() {
     document.querySelector("#month").innerHTML = months[monthIndex] + " " + currentYear;
     document.querySelector("#days").innerHTML = "";
     getDaysPlusWeekday(monthIndex, currentYear);
-};
+}
 
 function setLastMonth() {
     if (monthIndex === 0) {
@@ -274,47 +276,6 @@ function setLastMonth() {
     document.querySelector("#month").innerHTML = months[monthIndex] + " " + currentYear;
     document.querySelector("#days").innerHTML = "";
     getDaysPlusWeekday(monthIndex, currentYear);
-};
+}
 
-// Function to place the Days in the correct order 
-// and fill the empty spaces with 
-// the last month's days and the next month's days
-
-function fillCalendar() {
-    const days = getDaysInMonth(currentYear, monthIndex);
-    const lastMonthDays = getDaysInMonth(currentYear, monthIndex - 1);
-    const nextMonthDays = getDaysInMonth(currentYear, monthIndex + 1);
-
-    const weekDay = getWeekDay(currentYear, monthIndex, currentDay);
-    let output = "";
-
-    
-    for (let i = 1; i <= days; i++) {
-        const weekDay = getWeekDay(currentYear, monthIndex, i);
-        const date = i;
-        const day = weekDay + ", " + date;
-        const weekEnd = weekDay === "Saturday" || weekDay === "Sunday";
-        let output = weekEnd == true ?
-        `
-        <div class="border-r border-b border-slate-600 bg-slate-300">
-            <div class="py-1 px-3 border-b border-slate-600 bg-slate-400 text-gray-800 truncate">
-            ${day.split(",")[1]}.
-            ${day.split(",")[0]}
-            </div>
-            <div class="py-1 h-24 min-h-[12rem]"></div>
-        </div>`
-        :
-        `
-        <div class="border-r border-b border-slate-600 bg-slate-100">
-            <div class="py-1 px-3 border-b border-slate-600 bg-slate-300  truncate">
-                ${day.split(",")[1]}.
-                ${day.split(",")[0]}
-            </div>
-            <div class="py-1 min-h-[12rem] break-words">
-                "Here you can add some content"
-            </div>
-        </div>
-        `;
-        document.querySelector("#days").innerHTML += output;
-    };
-};
+console.log("Derzeit ist der Monat " + currentMonth + "\nDer Monat hat so viele Tage: " + daysInCurrentMonth + "\nDas ist unser Tag: " + currentDay  + "\nDas ist welcher Tag es in der Woche ist: " + weekDay);
